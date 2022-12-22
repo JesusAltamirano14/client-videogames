40e7b3a6fa50ff7637a84c4c306b206d555850c6
Peluchow <107145507+Peluchow@users.noreply.github.com>
2022-12-22 14:19:52 -0500

    redux devtools removed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducer from './redux/reducer';
-const store = createStore(reducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+// const store = createStore(reducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducer,applyMiddleware(thunk));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
